cdfbf85a7848bf271ceb047338c569a9f7c5469b
rperager <52241693+rperager@users.noreply.github.com>
2021-06-12 04:42:18 +0100

    Create TicketOffice.cpp

---
+++ TicketOffice.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+// You are working on a ticketing system. A ticket costs $10.
+// The office is running a discount campaign: each group of 5 people is getting a discount, which is a discount of the youngest age from the price of 5 tickets ($50 - youngest%).
+// You need to create a program that takes the ages of all 5 people as input and outputs the total price of the tickets.
+
+
+
+int main() {
+    int ages[5];
+    cout<<"Please input the ages of all of the people in your group of 5: ";
+    
+    for (int i = 0; i < 5; ++i) {
+        cin >> ages[i];
+    }
+    
+    int lowest = ages[0];
+
+    for(int i=0; i<5 ; i++){
+        if(i != 4){
+            if(ages[i+1] < lowest){
+                lowest = ages[i+1];
+                continue;
+            }
+        }else{
+            if (ages[4] < lowest){
+                lowest = ages[4];
+                break;
+            }
+        }
+    }
+    
+    double decrease = 0.01 * (100-lowest);
+    double discount = 50 * decrease;
+
+
+    cout<<"The price of the tickets is"<<discount<<endl;
+
+
+
+
+
+
+
+
+    return 0;
+}
