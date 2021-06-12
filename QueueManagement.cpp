5592e48d18b2846bae87ad9b3dcfc254d37e44ec
rperager <52241693+rperager@users.noreply.github.com>
2021-06-12 06:19:49 +0100

    QM patch 3
    Adding extra functionality; if queue reaches max limit, originally 100 to begin with, new queue with double the previous max limit is created and the contents of the old queue are copied in.

--- QueueManagement.cpp
+++ QueueManagement.cpp
@@ -10,11 +10,13 @@ using namespace std;
 class Queue { 
 	int size; 
 	int* queue; //int pointer 
+	int maxSize;
 	
 	public:
 	Queue() {
 		size = 0;
 		queue = new int[100];
+		maxSize = 100;
 	}
 	void remove() { 
 		if (size == 0) { 
@@ -43,6 +45,14 @@ class Queue {
 	  	if(size == 0){ 
 			queue[0] = item;
 			size++;
+			
+		}else if(size == maxSize){
+			maxSize *= 2;
+			int* oldQueue = queue;
+			queue = new int[maxSize];
+			for(int i=0; i<size; i++){
+				queue[i] = oldQueue[i];	
+			}
 		}else{
 			queue[size] = item;
 			size++;
